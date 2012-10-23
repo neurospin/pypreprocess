a754ee408df8ca06f8c48a77fc3c17b258cc6f44
dohmatob elvis dopgima <gmdopp@gmail.com>
2012-10-23 15:34:22 +0200

    fetch_local.py: fixed to pep8

--- fetch_local.py
+++ fetch_local.py
@@ -1,13 +1,8 @@
 """
-:Module: preproc_spm_haxby
-:Synopsis: SPM use-case for preprocessing haxby dataset (this is just a quick-and-dirty POC)
+:Module: preproc_spm_haxby\
+:Synopsis: SPM use-case for preprocessing haxby dataset\
+(this is just a quick-and-dirty POC)
 :Author: dohmatob elvis dopgima
-
-XXX TODO: document this according to numpy/spinx standards
-XXX TODO: re-factor the code (use unittesting)
-XXX TODO: visualization 
-XXX TODO: preprocessing checks
-XXX TODO: over-all testing (nose ?, see with GV & BT)
 
 """
 
@@ -15,8 +10,8 @@ XXX TODO: over-all testing (nose ?, see with GV & BT)
 import glob
 import gzip
 import os
-
 import re
+
 
 def unzip_nii_gz(dirname):
     """
@@ -31,9 +26,10 @@ def unzip_nii_gz(dirname):
             f_out.writelines(f_in)
             f_out.close()
             f_in.close()
-            # os.remove(filename) # XXX why ?
+            # os.remove(filename) #  XXX why ?
 
-def fetch_haxby_data_offline(haxby_dir, session_ids=["haxby2001"], 
+
+def fetch_haxby_data_offline(haxby_dir, session_ids=["haxby2001"],
                              subject_ids=["subj1", "subj2", "subj3", "subj4"]):
     """
     Helper function for globbing local haxby directory structure.
@@ -41,31 +37,31 @@ def fetch_haxby_data_offline(haxby_dir, session_ids=["haxby2001"],
     """
     assert os.path.exists(haxby_dir)
 
-    print "Pulling HAXBY data from %s .."%haxby_dir
+    print "Pulling HAXBY data from %s .." % haxby_dir
     sessions = dict()
     for session_id in session_ids:
-        session_dir = os.path.join(haxby_dir, "%s"%session_id)
+        session_dir = os.path.join(haxby_dir, "%s" % session_id)
 
         if not os.path.exists(session_dir):
-            print "Warning: %s doesn't exist"%session_dir
+            print "Warning: %s doesn't exist" % session_dir
             continue
 
         sessions[session_id] = dict()
 
         for subject_id in subject_ids:
-            subject_dir = os.path.join(session_dir, "%s"%subject_id)
+            subject_dir = os.path.join(session_dir, "%s" % subject_id)
             if not os.path.exists(subject_dir):
-                print "Warning: %s doesn't exist"%subject_dir
+                print "Warning: %s doesn't exist" % subject_dir
                 continue
 
             # Because SPM doesn't understand .gz
-            print '\t\tDoing .nii.gz -> .nii extraction in %s'%subject_dir
+            print '\t\tDoing .nii.gz -> .nii extraction in %s' % subject_dir
             unzip_nii_gz(subject_dir)
             print '\t\tDone.'
 
             try:
-                anat_image = glob.glob("%s/anat.nii"%subject_dir)[0]
-                fmri_image = glob.glob("%s/bold.nii"%subject_dir)[0]
+                anat_image = glob.glob("%s/anat.nii" % subject_dir)[0]
+                fmri_image = glob.glob("%s/bold.nii" % subject_dir)[0]
             except IndexError:
                 continue
 
@@ -75,32 +71,34 @@ def fetch_haxby_data_offline(haxby_dir, session_ids=["haxby2001"],
 
         if len(sessions[session_id]) == 0:
             del sessions[session_id]
-        
-    print "Done. Pulled data for %d sesssion(s)."%len(sessions)
+
+    print "Done. Pulled data for %d sesssion(s)." % len(sessions)
     return sessions
 
-def fetch_nyu_data_offline(nyu_rest_dir, session_ids=["session1"], subject_ids=["sub05676"]):
+
+def fetch_nyu_data_offline(nyu_rest_dir, session_ids=["session1"],
+                           subject_ids=["sub05676"]):
     """
     Helper function for globbing local NYU directory structure.
 
     """
     assert os.path.exists(nyu_rest_dir)
 
-    print "Pulling NYU (rest) data from %s .."%nyu_rest_dir
+    print "Pulling NYU (rest) data from %s .." % nyu_rest_dir
     sessions = dict()
     for session_id in session_ids:
-        session_dir = os.path.join(nyu_rest_dir, "%s"%session_id)
+        session_dir = os.path.join(nyu_rest_dir, "%s" % session_id)
 
         if not os.path.exists(session_dir):
-            print "Warning: %s doesn't exist"%session_dir
+            print "Warning: %s doesn't exist" % session_dir
             continue
 
         sessions[session_id] = dict()
 
         for subject_id in subject_ids:
-            subject_dir = os.path.join(session_dir, "%s"%subject_id)
+            subject_dir = os.path.join(session_dir, "%s" % subject_id)
             if not os.path.exists(subject_dir):
-                print "Warning: %s doesn't exist"%subject_dir
+                print "Warning: %s doesn't exist" % subject_dir
                 continue
 
             # Because SPM doesn't understand .gz
@@ -108,19 +106,23 @@ def fetch_nyu_data_offline(nyu_rest_dir, session_ids=["session1"], subject_ids=[
             unzip_nii_gz(os.path.join(subject_dir, "func"))
 
             try:
-                anonymized_image = glob.glob("%s/anat/mprage_anonymized.nii"%subject_dir)[0]
-                skullstripped_image = glob.glob("%s/anat/mprage_skullstripped.nii"%subject_dir)[0]
-                fmri_image = glob.glob("%s/func/lfo.nii"%subject_dir)[0]
+                anonymized_image = glob.glob("%s/anat/mprage_anonymized.nii" %\
+                                                 subject_dir)[0]
+                skullstripped_image = glob.glob(
+                    "%s/anat/mprage_skullstripped.nii" % subject_dir)[0]
+                fmri_image = glob.glob("%s/func/lfo.nii" % subject_dir)[0]
             except IndexError:
                 continue
 
             sessions[session_id][subject_id] = dict()
-            sessions[session_id][subject_id]['skullstripped_anat'] = skullstripped_image
-            sessions[session_id][subject_id]['anonymized_anat'] = anonymized_image
+            sessions[session_id][subject_id]['skullstripped_anat'] = \
+                skullstripped_image
+            sessions[session_id][subject_id]['anonymized_anat'] = \
+                anonymized_image
             sessions[session_id][subject_id]['func'] = fmri_image
 
         if len(sessions[session_id]) == 0:
             del sessions[session_id]
-        
-    print "Done. Pulled data for %d sesssion(s)"%len(sessions)
+
+    print "Done. Pulled data for %d sesssion(s)" % len(sessions)
     return sessions
