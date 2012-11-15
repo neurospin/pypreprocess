f32d92d7969fadbfc4e5eaf3843f897bf04f2c7f
DOP <gmdopp@gmail.com>
2012-11-15 08:05:36 +0100

    nipype_preproc_spm_nyu.py: refactored

--- nipype_preproc_spm_nyu.py
+++ nipype_preproc_spm_nyu.py
@@ -27,18 +27,13 @@ from joblib import Parallel, delayed
 from multiprocessing import cpu_count
 
 # QA imports
-from check_preprocessing import *
-import markup
-from report_utils import *
+from reporter import BASE_PREPROC_REPORT_HTML_TEMPLATE
 import time
 
 # set data dir
 if not 'DATA_DIR' in os.environ:
     raise IOError, "DATA_DIR is not in your environ; export it!"
 DATA_DIR = os.environ['DATA_DIR']
-
-# set interesting subject ids
-SUBJECT_IDS = ["sub05676", "sub08889", "sub14864", "sub08224"]
 
 # set job count
 N_JOBS = -1
@@ -78,85 +73,25 @@ if __name__ == '__main__':
                                       for args in preproc_factory())
 
     # generate html report (for QA)
-    report_filename = os.path.join(DATA_DIR, "_report.html")
-    report = markup.page(mode="strict_html")
-    report.h1(""" pypreproc run, %s.""" % time.asctime())
-    report.p("See reports for each stage below.")
-    plots = dict()
-    plots["cv_tc"] = list()
-    plots["realignment"] = list()
-    plots["coregistration"] = list()
-    plots["segmentation"] = list()
+    blablabla = "Generating QA report for %d subjects .." % len(results)
+    dadada = "+" * len(blablabla)
+    print "\r\n%s\r\n%s\r\n%s\r\n" % (dadada, blablabla, dadada)
+
+    tmpl = BASE_PREPROC_REPORT_HTML_TEMPLATE
+    report_filename = "nyu_preproc_report.html"
+    plots_gallery = list()
 
     for subject_id, session_id, output in results:
-        report.a("Report for subject %s " % subject_id, class_="internal",
-                 href=output["report"])
-        plots["cv_tc"].append(output["plots"]["cv_tc"])
-        plots["realignment"].append(output["plots"]["realignment_parameters"])
-        plots["coregistration"].append(output["plots"]["coregistration"])
-        plots["segmentation"].append(output["plots"]["segmentation"])
+        full_plot = output['plots']['segmentation']
+        title = 'subject: %s' % subject_id
+        summary_plot = output['plots']['segmentation_summary']
+        redirect_url = output["report"]
+        plots_gallery.append((full_plot, title, summary_plot, redirect_url))
 
-    report.h2("Grouped reports")
-
-    report.a("Coefficient of Variation report", class_='internal',
-             href="cv_tc_report.html")
-    report.br()
-    cv_tc_report_filename = os.path.join(DATA_DIR,
-                                                "cv_tc_report.html")
-    cv_tc_report = markup.page(mode='loose_html')
-    cv_tc_report.h1(
-        "Coefficient of Variation report for %i subjects" % len(results))
-    sidebyside(cv_tc_report,
-               plots["cv_tc"][:len(plots["cv_tc"]) / 2],
-               plots["cv_tc"][len(plots["cv_tc"]) / 2:])
-    with open(cv_tc_report_filename, 'w') as fd:
-        fd.write(str(cv_tc_report))
-        fd.close()
-
-    report.a("Realignment report", class_='internal',
-             href="realignment_report.html")
-    report.br()
-    realignment_report_filename = os.path.join(DATA_DIR,
-                                                "realignment_report.html")
-    realignment_report = markup.page(mode='loose_html')
-    realignment_report.h1("Realignment report for %i subjects" % len(results))
-    sidebyside(realignment_report,
-               plots["realignment"][:len(plots["realignment"]) / 2],
-               plots["realignment"][len(plots["realignment"]) / 2:])
-    with open(realignment_report_filename, 'w') as fd:
-        fd.write(str(realignment_report))
-        fd.close()
-
-    report.a("Coregistration report", class_='internal',
-             href="coregistration_report.html")
-    report.br()
-    coregistration_report_filename = os.path.join(DATA_DIR,
-                                                "coregistration_report.html")
-    coregistration_report = markup.page(mode='loose_html')
-    coregistration_report.h1(
-        "Coregistration report for %i subjects" % len(results))
-    sidebyside(coregistration_report,
-               plots["coregistration"][:len(plots["coregistration"]) / 2],
-               plots["coregistration"][len(plots["coregistration"]) / 2:])
-    with open(coregistration_report_filename, 'w') as fd:
-        fd.write(str(coregistration_report))
-        fd.close()
-
-    report.a("Segmentation report", class_='internal',
-             href="segmentation_report.html")
-    report.br()
-    segmentation_report_filename = os.path.join(DATA_DIR,
-                                                "segmentation_report.html")
-    segmentation_report = markup.page(mode='loose_html')
-    segmentation_report.h1(
-        "Segmentation report for %i subjects" % len(results))
-    sidebyside(segmentation_report,
-               plots["segmentation"][:len(plots["segmentation"]) / 2],
-               plots["segmentation"][len(plots["segmentation"]) / 2:])
-    with open(segmentation_report_filename, 'w') as fd:
-        fd.write(str(segmentation_report))
-        fd.close()
-
+    report  = tmpl.substitute(now=time.ctime(), plots_gallery=plots_gallery)
+    print report
     with open(report_filename, 'w') as fd:
         fd.write(str(report))
         fd.close()
+
+    print "\r\nDone."
