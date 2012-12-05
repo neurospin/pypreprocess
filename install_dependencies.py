57fb5fa4d3fac86710a576ad56a4d8864c0dd6f2
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2012-12-05 08:34:10 +0100

    install_dependencies.py: added possibility to delegate cmd-line options to pip

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,5 +1,23 @@
+"""
+:Module: install_dependencies.py
+:Synopsis: Simple dependency-installer for pypreprocess.
+:Author: dohmatob elvis dopgima
+
+"""
+
 import os
+import sys
 
 if __name__ == '__main__':
-    os.system('pip install -r urgent_dependencies.txt --user')
-    os.system('pip install -r dependencies.txt --user')
+    other_options_for_pip = "--user"
+    if len(sys.argv) > 1:
+        other_options_for_pip = " ".join(sys.argv[1:])
+
+    cmd1 = 'pip install -r urgent_dependencies.txt %s' % other_options_for_pip
+    cmd2 = 'pip install -r dependencies.txt %s' % other_options_for_pip
+
+    print "Running: %s\r\n\r\n" % cmd1
+    os.system(cmd1)
+
+    print "Running: %s\r\n" % cmd2
+    os.system(cmd2)
