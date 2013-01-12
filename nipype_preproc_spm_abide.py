8aee88289b4643057b49a5d672b23540f70c71a3
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2013-01-12 06:41:24 +0100

    nipype_preproc_spm_abide.py: DARTEL-less stable script for preprocessing ABIDE dataset

--- nipype_preproc_spm_abide.py
+++ nipype_preproc_spm_abide.py
@@ -9,6 +9,7 @@
 import os
 import glob
 import sys
+import random
 
 # import spm preproc utilities
 import nipype_preproc_spm_utils
@@ -18,7 +19,7 @@ DATASET_DESCRIPTION = """\
 """
 
 # XXX change this to */* if preprocessing all ABIDE subjects
-subject_id_wildcard = "UM_*/UM_*"
+subject_id_wildcard = "*_*/*_*"
 
 if __name__ == '__main__':
     # sanitize cmd-line input
@@ -35,28 +36,63 @@ if __name__ == '__main__':
     if not os.path.isdir(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
+    if len(sys.argv) > 3:
+        subject_id_wildcard = sys.argv[3]
+
     # glob for subject ids
     subject_ids = [os.path.basename(x)
                    for x in glob.glob(
             os.path.join(ABIDE_DIR, subject_id_wildcard))]
+    ignored_subject_ids = []
+
+    random.shuffle(subject_ids)
+
+    def ignore_subject_id(subject_id):
+        return os.path.exists(os.path.join(
+                OUTPUT_DIR, "UNKNOWN_SESSION/%s/final" % subject_id))
 
     # producer subject data
     def subject_factory():
         for subject_id in subject_ids:
+            if ignore_subject_id(subject_id):
+                continue
+
             subject_data = nipype_preproc_spm_utils.SubjectData()
             subject_data.subject_id = subject_id
 
-            subject_data.func = glob.glob(
-                os.path.join(
-                    ABIDE_DIR,
-                    "%s/%s/scans/rest/resources/NIfTI/files/rest.nii" % (
-                        subject_id, subject_id)))
+            try:
+                subject_data.func = glob.glob(
+                    os.path.join(
+                        ABIDE_DIR,
+                        "%s/%s/scans/rest*/resources/NIfTI/files/rest.nii" % (
+                            subject_id, subject_id)))[0]
+            except IndexError:
+                ignored_because = "no rest data found"
+                print "Ignoring subject %s (%s)" % (subject_id,
+                                                    ignored_because)
+                ignored_subject_ids.append((subject_id, ignored_because))
+                continue
 
-            subject_data.anat = glob.glob(
-                os.path.join(
-                    ABIDE_DIR,
-                    "%s/%s/scans/anat/resources/NIfTI/files/mprage.nii" % (
-                        subject_id, subject_id)))
+            try:
+                subject_data.anat = glob.glob(
+                    os.path.join(
+                        ABIDE_DIR,
+                        "%s/%s/scans/anat/resources/NIfTI/files/mprage.nii" % (
+                            subject_id, subject_id)))[0]
+            except IndexError:
+                try:
+                    subject_data.hires = glob.glob(
+                        os.path.join(
+                            ABIDE_DIR,
+                            ("%s/%s/scans/hires/resources/NIfTI/"
+                             "files/hires.nii") % (
+                                subject_id, subject_id)))[0]
+                except IndexError:
+                    ignored_because = "no anat/hires data found"
+                    print "Ignoring subject %s (%s)" % (subject_id,
+                                                        ignored_because)
+                    ignored_subject_ids.append((subject_id, ignored_because))
+                    continue
 
             subject_data.output_dir = os.path.join(
                 os.path.join(
@@ -71,6 +107,9 @@ if __name__ == '__main__':
     nipype_preproc_spm_utils.do_group_preproc(
         subject_factory(),
         delete_orientation=True,
-        do_export_report=False,
+        # do_export_report=True,
         dataset_description=DATASET_DESCRIPTION,
         report_filename=report_filename)
+
+    for subject_id, ignored_because in ignored_subject_ids:
+        print "Ignored %s because %s" % (subject_id, ignored_because)
