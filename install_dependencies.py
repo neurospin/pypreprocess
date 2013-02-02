62c3fda781277241d0ba77473c72234adb0ec116
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2013-02-02 17:54:32 +0100

    added nisl dependency

--- install_dependencies.py
+++ install_dependencies.py
@@ -13,11 +13,20 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         other_options_for_pip = " ".join(sys.argv[1:])
 
+    os.chdir(os.path.dirname(sys.argv[0]))
+
     cmd1 = 'pip install -r urgent_dependencies.txt %s' % other_options_for_pip
-    cmd2 = 'pip install -r dependencies.txt %s' % other_options_for_pip
+
+    cmd2 = 'pip install -r questionable_dependencies.txt %s' \
+        % other_options_for_pip
+
+    cmd3 = 'pip install -r dependencies.txt %s' % other_options_for_pip
 
     print "Running: %s\r\n\r\n" % cmd1
     os.system(cmd1)
 
     print "Running: %s\r\n" % cmd2
     os.system(cmd2)
+
+    print "Running: %s\r\n" % cmd3
+    os.system(cmd3)
