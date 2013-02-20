e3e1b12490ff1e6a3c14fb6d90229913e8daefc4
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2013-02-20 15:28:31 +0100

    ENH: stats report looks much better now

--- examples/nipy_glm_spm_auditory.py
+++ examples/nipy_glm_spm_auditory.py
@@ -1,6 +1,6 @@
-B"""
-:1;3201;0c1;3201;0cSynopsis: Demo script for nipy's GLM tools
-:1;3201;0cAuthor: dohmatob elvis dopgima
+"""
+Synopsis: Demo script for nipy's GLM tools
+Author: dohmatob elvis dopgima
 
 """
 
@@ -13,6 +13,7 @@ from nipy.modalities.fmri.design_matrix import make_dmtx
 from nipy.modalities.fmri.glm import FMRILinearModel
 from nipy.labs import viz
 import nibabel
+import time
 
 sys.path.append(
     os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
@@ -26,6 +27,9 @@ DATASET_DESCRIPTION = """\
 <p>MoAEpilot <a href="http://www.fil.ion.ucl.ac.uk/spm/data/auditory/">\
 SPM auditory dataset</a>.</p>\
 """
+
+"""MISC"""
+NIPY_URL = "http://nipy.sourceforge.net/nipy/stable/index.html"
 
 if len(sys.argv)  < 3:
     print ("\r\nUsage: python %s <spm_auditory_MoAEpilot_dir>"
@@ -64,21 +68,23 @@ results = nipype_preproc_spm_utils.do_subjects_preproc(
 progress_logger = results[0]['progress_logger']
 stats_report_filename = os.path.join(subject_data.output_dir,
                                      "report_stats.html")
-# level1_loader_filename = os.path.join(
-#     subject_data.output_dir, "level1_thumbs.html")
-# level1_thumbs = reporter.ResultsGallery(
-#     loader_filename=level1_loader_filename,
-#     )
-# import time
-# level1_html_markup = reporter.FSL_SUBJECT_REPORT_PREPROC_HTML_TEMPLATE(
-#     ).substitute(
-#     results=level1_thumbs,
-#     start_time=time.ctime(),
-#     subject_id=subject_data.subject_id
-#     )
-# with open(stats_report_filename, 'w') as fd:
-#     fd.write(str(level1_html_markup))
-#     fd.close()
+progress_logger.watch_file(stats_report_filename)
+level1_loader_filename = os.path.join(
+    subject_data.output_dir, "level1_thumbs.html")
+level1_thumbs = reporter.ResultsGallery(
+    loader_filename=level1_loader_filename,
+    )
+level1_html_markup = reporter.FSL_SUBJECT_REPORT_STATS_HTML_TEMPLATE(
+    ).substitute(
+    results=level1_thumbs,
+    start_time=time.ctime(),
+    subject_id=subject_data.subject_id,
+    methods=('GLM and inference have been done using '
+             '<a href="%s">nipy</a>.') % NIPY_URL,
+             )
+with open(stats_report_filename, 'w') as fd:
+    fd.write(str(level1_html_markup))
+    fd.close()
 progress_logger.log("<b>Level 1 statistics</b><br/><br/>")
 
 """collect preprocessed data"""
@@ -86,6 +92,7 @@ fmri_data = results[0]['func']
 fmri_data = do_3Dto4D_merge(fmri_data)
 
 """construct experimental paradigm"""
+progress_logger.log("Computing design matrix ..<br/>")
 tr = 7
 n_scans = 96
 _duration = 6
@@ -110,14 +117,14 @@ ax = design_matrix.show()
 ax.set_position([.05, .25, .9, .65])
 ax.set_title('Design matrix')
 dmat_outfile = os.path.join(subject_data.output_dir, 'design_matrix.png')
-pl.savefig(dmat_outfile)
-# thumb = reporter.Thumbnail()
-# thumb.a = reporter.a(href=os.path.basename(dmat_outfile))
-# thumb.img = reporter.img(src=os.path.basename(dmat_outfile),
-#                          height="250px",
-#                          width="600px")
-# thumb.description = "DMAT"
-# level1_thumbs.commit_thumbnails(thumb)
+pl.savefig(dmat_outfile, bbox_inches="tight", dpi=200)
+thumb = reporter.Thumbnail()
+thumb.a = reporter.a(href=os.path.basename(dmat_outfile))
+thumb.img = reporter.img(src=os.path.basename(dmat_outfile),
+                         height="500px",
+                         width="600px")
+thumb.description = "Design Matrix"
+level1_thumbs.commit_thumbnails(thumb)
 
 """specify contrasts"""
 contrasts = {}
@@ -142,7 +149,7 @@ nibabel.save(fmri_glm.mask, mask_path)
 
 """level 1 (within subject) inference"""
 print "Computing contrasts .."
-progress_logger.log("Computing contrats ..<br/>")
+progress_logger.log("Computing contrasts ..<br/>")
 for contrast_id in contrasts:
     print "\tcontrast id: %s" % contrast_id
     z_map, t_map, eff_map, var_map = fmri_glm.contrast(
@@ -154,24 +161,39 @@ for contrast_id in contrasts:
         output_variance=True,)
 
     if contrast_id == 'active-rest':
-        z_threshold = 5
+        z_threshold = 5.
+        z_axis_max = np.unravel_index(
+            z_map.get_data().argmax(), z_map.shape)[2]
+        z_axis_min = np.unravel_index(
+            z_map.get_data().argmin(), z_map.shape)[2]
         pos_data = z_map.get_data() * (z_map.get_data() > 0)
         vmax = pos_data.max()
         vmin = - vmax
 
         viz.plot_map(pos_data, z_map.get_affine(),
-                     cmap=pl.cm.spectral,
+                     cmap=pl.cm.hot,
                      vmin=vmin,
                      vmax=vmax,
                      threshold=z_threshold,
                      slicer='z',
-                     cut_coords=range(-17, 20, 3))
+                     cut_coords=range(z_axis_min, z_axis_max + 1, 2),
+                     black_bg=True,
+                     )
 
-        reporter.generate_level1_report(
-            z_map, fmri_glm.mask,
-            stats_report_filename, threshold=0.001,
-            cluster_th=50)
+        z_map_plot = os.path.join(subject_data.output_dir,
+                                  "activation_map.png")
+        stats_table = os.path.join(subject_data.output_dir, "stats_table.html")
+        pl.savefig(z_map_plot, dpi=200, bbox_inches='tight',
+                   facecolor="k",
+                   edgecolor="k")
 
+        thumbnail = reporter.Thumbnail()
+        thumbnail.a = reporter.a(href=os.path.basename(z_map_plot))
+        thumbnail.img = reporter.img(
+            src=os.path.basename(z_map_plot), height="250px")
+        thumbnail.description = "Activation z-map (thresholded at %s)"\
+            % z_threshold
+        level1_thumbs.commit_thumbnails(thumbnail)
 
     for dtype, out_map in zip(['z', 't', 'effects', 'variance'],
                               [z_map, t_map, eff_map, var_map]):
@@ -183,6 +205,13 @@ for contrast_id in contrasts:
             map_dir, '%s.nii.gz' % contrast_id)
         nibabel.save(out_map, map_path)
 
+        if contrast_id == "active-rest" and dtype == "z":
+            reporter.generate_level1_report(
+                z_map, fmri_glm.mask,
+                stats_table,
+                title="%s z-map: %s" % (contrast_id, map_path),
+                cluster_th=50)
+
         print "\t\t%s map: %s" % (dtype, map_path)
 
     print
@@ -193,4 +222,4 @@ progress_logger.log('<hr/>')
 progress_logger.finish_all()
 
 """show all generated plots this far"""
-pl.show()
+# pl.show()
