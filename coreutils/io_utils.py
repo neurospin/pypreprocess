3375408b650203e3362f9f31d8df6dea972c53a5
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2013-07-22 18:08:29 +0200

    utility dir

---
+++ coreutils/io_utils.py
@@ -0,0 +1,367 @@
+"""
+:Module: utils
+:Synopsis: routine business related to image i/o manips
+:Author: dohmatob elvis dopgima
+
+"""
+
+import os
+import joblib
+import commands
+import tempfile
+import numpy as np
+import nibabel
+from external.nilearn import resampling
+
+
+def _load_vol(x):
+    """
+    Loads a single 3D volume.
+
+    """
+
+    if isinstance(x, basestring):
+        vol = nibabel.load(x)
+    elif isinstance(x, nibabel.Nifti1Image) or isinstance(
+        x, nibabel.Nifti1Pair):
+        vol = x
+    else:
+        raise TypeError(
+            ("Each volume must be string, image object, got:"
+             " %s") % type(x))
+
+    if len(vol.shape) == 4:
+        if vol.shape[-1] == 1:
+            vol = nibabel.Nifti1Image(vol.get_data()[..., 0],
+                                      vol.get_affine())
+        else:
+            raise ValueError(
+                "Each volume must be 3D, got %iD" % len(vol.shape))
+    elif len(vol.shape) != 3:
+            raise ValueError(
+                "Each volume must be 3D, got %iD" % len(vol.shape))
+
+    return vol
+
+
+def _load_specific_vol(vols, t):
+    """Utility function for loading specific volume on demand.
+
+    """
+
+    assert t >= 0
+
+    if isinstance(vols, list):
+        n_scans = len(vols)
+        vol = _load_vol(vols[t])
+    elif isinstance(vols, nibabel.Nifti1Image) or isinstance(
+        vols, nibabel.Nifti1Pair) or isinstance(vols, basestring):
+        _vols = nibabel.load(vols) if isinstance(vols, basestring) else vols
+        if len(_vols.shape) != 4:
+            raise ValueError(
+                "Expecting 4D image, got %iD" % len(_vols.shape))
+
+        n_scans = _vols.shape[3]
+        vol = nibabel.four_to_three(_vols)[t]
+    else:  # unhandled type
+        raise TypeError(
+            "imgs arg must be string, image object, or list of such.")
+
+    return vol, n_scans
+
+
+def _save_vols(vols, output_dir, concat=False, prefix='', ext='.nii'):
+    """
+    Saves a single 4D image or a couple of 3D vols unto disk.
+
+    vols: single 4D nibabel image object, or list of 3D nibabel image objects
+        volumes to be saved
+    output_dir: string
+        existing filename, destination directory
+    prefix: string, optional (default '')
+       prefix to be prepended to output file basenames
+
+    """
+
+    if isinstance(vols, nibabel.Nifti1Image) or isinstance(
+        vols, nibabel.Nifti1Pair) or concat:
+        if concat:
+            vols = nibabel.concat_images(vols)
+
+        filenames = os.path.join(output_dir, "%s%s" % (prefix, ext))
+        nibabel.save(vols, filenames)
+
+        return filenames
+    else:
+        n_vols = len(vols)
+        filenames = []
+        if prefix:
+            prefix = prefix + "_"
+        for t, vol in zip(xrange(n_vols), vols):
+            # save realigned vol unto disk
+            output_filename = os.path.join(output_dir,
+            "%svol_%i%s" % (
+                prefix, t, ext))
+            nibabel.save(vol, output_filename)
+
+            # update rvols and filenames
+            filenames.append(output_filename)
+
+    return filenames
+
+
+def is_3D(image):
+    """Check whether image is 3D"""
+
+    if isinstance(image, basestring):
+        image = nibabel.load(image)
+    elif isinstance(image, list):
+        image = nibabel.concat_images(image)
+
+    if len(image.shape) == 3:
+        return True
+    else:
+        return len(image.shape) == 4 and image.shape[-1] == 1
+
+
+def is_4D(image):
+    """Check whether image is 4D
+    """
+
+    if isinstance(image, basestring):
+        image = nibabel.load(image)
+
+    if len(image.shape) == 4:
+        return True
+    else:
+        return len(image.shape) == 5 and image.shape[-1] == 1
+
+
+def get_vox_dims(volume):
+    """
+    Infer voxel dimensions of a nifti image.
+
+    Parameters
+    ----------
+    volume: string
+        image whose voxel dimensions we seek
+
+    """
+
+    if not isinstance(volume, basestring):
+        volume = volume[0]
+    try:
+        nii = nibabel.load(volume)
+    except:
+        # XXX quick and dirty
+        nii = nibabel.concat_images(volume)
+
+    hdr = nii.get_header()
+    voxdims = hdr.get_zooms()
+
+    return [float(voxdims[0]), float(voxdims[1]), float(voxdims[2])]
+
+
+def delete_orientation(imgs, output_dir, output_tag=''):
+    """Function to delete (corrupt) orientation meta-data in nifti
+
+    XXX TODO: Do this without using fsl
+
+    Parameters
+    ----------
+    imgs: string or list of string
+       path (paths) to 4D (3D) image (images) under inspection
+
+    output_dir: string
+       directory to which output will be written
+
+    output_tag: string (optional)
+       tag to append output image filename(s) with
+
+    Returns
+    -------
+    output images
+
+    """
+
+    output_imgs = []
+    not_list = False
+    if not type(imgs) is list:
+        not_list = True
+        imgs = [imgs]
+
+    for img in imgs:
+        output_img = os.path.join(
+            output_dir,
+            "deleteorient_%s_" % (output_tag) + os.path.basename(img))
+        nibabel.save(nibabel.load(img), output_img)
+        print commands.getoutput(
+            "fslorient -deleteorient %s" % output_img)
+        print "+++++++Done (deleteorient)."
+        print "Deleted orientation meta-data %s." % output_img
+        output_imgs.append(output_img)
+
+    if not_list:
+        output_imgs = output_imgs[0]
+
+    return output_imgs
+
+
+def do_3Dto4D_merge(
+    threeD_img_filenames,
+    output_dir=None,
+    output_filename=None):
+    """
+    This function produces a single 4D nifti image from several 3D.
+
+    threeD_img_filenames: list of string
+        paths to images to be merged
+
+    Returns
+    -------
+    returns nifit image object
+
+    """
+
+    if isinstance(threeD_img_filenames, basestring):
+        return nibabel.load(threeD_img_filenames)
+
+    if output_dir is None:
+        output_dir = tempfile.mkdtemp()
+
+    # prepare for smart caching
+    merge_cache_dir = os.path.join(output_dir, "merge")
+    if not os.path.exists(merge_cache_dir):
+        os.makedirs(merge_cache_dir)
+    merge_mem = joblib.Memory(cachedir=merge_cache_dir, verbose=5)
+
+    # merging proper
+    fourD_img = merge_mem.cache(nibabel.concat_images)(threeD_img_filenames)
+
+    # sanity
+    if len(fourD_img.shape) == 5:
+        fourD_img = nibabel.Nifti1Image(
+            fourD_img.get_data()[..., ..., ..., 0, ...],
+            fourD_img.get_affine())
+
+    # save image to disk
+    if not output_filename is None:
+        merge_mem.cache(nibabel.save)(fourD_img, output_filename)
+
+    return fourD_img
+
+
+def resample_img(input_img_filename,
+                 new_vox_dims, output_img_filename=None):
+    """Resamples an image to a new resolution
+
+    Parameters
+    ----------
+    input_img_filename: string
+        path to image to be resampled
+
+    new_vox_dims: list or tuple of +ve floats
+        new vox dimensions to which the image is to be resampled
+
+    output_img_filename: string (optional)
+        where output image will be written
+
+    Returns
+    -------
+    output_img_filename: string
+        where the resampled img has been written
+
+    """
+
+    # sanity
+    if output_img_filename is None:
+        output_img_filename = os.path.join(
+            os.path.dirname(input_img_filename),
+            "resample_" + os.path.basename(input_img_filename))
+
+    # prepare for smart-caching
+    output_dir = os.path.dirname(output_img_filename)
+    cache_dir = os.path.join(output_dir, "resample_img_cache")
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+    mem = joblib.Memory(cachedir=cache_dir, verbose=5)
+
+    # resample input img to new resolution
+    resampled_img = mem.cache(resampling.resample_img)(
+        input_img_filename,
+        target_affine=np.diag(new_vox_dims))
+
+    # save resampled img
+    nibabel.save(resampled_img, output_img_filename)
+
+    return output_img_filename
+
+
+def compute_mean_image(images, output_filename=None, threeD=False):
+    """Computes the mean of --perhaps differently shaped-- images
+
+    Parameters
+    ----------
+    images: string/image object, or list (-like) of
+        image(s) whose mean we seek
+
+    Returns
+    -------
+    mean nifti image object
+
+    """
+
+    # sanitize
+    if not hasattr(images, '__iter__') or isinstance(images, basestring):
+        images = [images]
+
+    # make list of data an affines
+    all_data = []
+    all_affine = []
+    for image in images:
+        if isinstance(image, basestring):
+            image = nibabel.load(image)
+        else:
+            image = nibabel.concat_images(image)
+            # raise IOError(type(image))
+        data = image.get_data()
+
+        if threeD:
+            if is_4D(image):
+                data = data.mean(-1)
+
+        all_data.append(data)
+        all_affine.append(image.get_affine())
+
+    # compute mean
+    mean_data = np.mean(all_data, axis=0)
+
+    # XXX I'm assuming all the affines are equal
+    mean_affine = all_affine[0]
+
+    mean_image = nibabel.Nifti1Image(mean_data, mean_affine)
+
+    # save mean image
+    if output_filename:
+        nibabel.save(mean_image, output_filename)
+
+    # return return result
+    return mean_image
+
+
+def compute_mean_3D_image(images, output_filename=None):
+    """Computes the mean of --perhaps differently shaped-- images
+
+    Parameters
+    ----------
+    images: string/image object, or list (-like) of
+        image(s) whose mean we seek
+
+    Returns
+    -------
+    mean nifti image object
+
+    """
+
+    return compute_mean_image(images, output_filename=output_filename,
+                              threeD=True)
---
+++ coreutils/tests/test_io_utils.py
@@ -0,0 +1,166 @@
+import sys
+import numpy as np
+import nibabel
+import os
+import inspect
+import nose
+import nose.tools
+
+# pypreproces path
+PYPREPROCESS_DIR = os.path.dirname(os.path.dirname(
+        os.path.split(os.path.abspath(__file__))[0]))
+sys.path.append(PYPREPROCESS_DIR)
+
+# import the APIIS to be tested
+from coreutils.io_utils import (
+    _load_vol,
+    _load_specific_vol,
+    do_3Dto4D_merge,
+    _save_vols)
+
+# global setup
+this_file = os.path.basename(os.path.abspath(__file__)).split('.')[0]
+OUTPUT_DIR = "/tmp/%s" % this_file
+IMAGE_EXTENSIONS = [".nii", ".nii.gz", ".img"]
+
+
+def create_random_image(shape=None,
+                        ndim=3,
+                        n_scans=None,
+                        affine=np.eye(4),
+                        parent_class=nibabel.Nifti1Image):
+    """
+    Creates a random image of prescribed shape
+
+    """
+
+    if shape is None:
+        shape = np.random.random_integers(20, size=ndim)
+
+    ndim = len(shape)
+    if not n_scans is None and ndim == 4:
+        shape[-1] = n_scans
+
+    return parent_class(np.random.randn(*shape), affine)
+
+
+def test_load_vol():
+    # setup
+    output_dir = os.path.join(OUTPUT_DIR, inspect.stack()[0][3])
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # creat a volume
+    vol = create_random_image()
+
+    # test loading vol from nibabel object
+    _vol = _load_vol(vol)
+    nose.tools.assert_true(isinstance(_vol, type(vol)))
+    nose.tools.assert_equal(_vol.shape, vol.shape)
+    np.testing.assert_array_equal(_vol.get_data(), vol.get_data())
+
+    # test loading vol by filename
+    for ext in IMAGE_EXTENSIONS:
+        # save vol with extension ext
+        vol_filename = os.path.join(output_dir, "vol%s" % ext)
+        nibabel.save(vol, vol_filename)
+
+        # note that .img loads as Nifti1Pair, not Nifti1Image
+        vol_type = nibabel.Nifti1Pair if ext == '.img' else nibabel.Nifti1Image
+
+        # load the vol by filename
+        _vol = _load_vol(vol_filename)
+        nose.tools.assert_true(isinstance(_vol, vol_type))
+        nose.tools.assert_equal(_vol.shape, vol.shape)
+        np.testing.assert_array_equal(_vol.get_data(), vol.get_data())
+
+
+def test_load_specific_vol():
+    # setup
+    output_dir = os.path.join(OUTPUT_DIR, inspect.stack()[0][3])
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    n_scans = 23
+
+    # create 4D film
+    film = create_random_image(ndim=4, n_scans=n_scans)
+
+    # test loading vol from nibabel image object
+    for t in xrange(n_scans):
+        _vol, _n_scans = _load_specific_vol(film, t)
+        nose.tools.assert_equal(_n_scans, n_scans)
+        nose.tools.assert_true(isinstance(_vol, type(film)))
+        nose.tools.assert_equal(_vol.shape, film.shape[:-1])
+        np.testing.assert_array_equal(_vol.get_data(), film.get_data()[..., t])
+
+    # test loading vol from a single 4D filename
+    for ext in IMAGE_EXTENSIONS:
+        for film_filename_type in ['str', 'list']:
+            if film_filename_type == 'str':
+                # save film as single filename with extension ext
+                film_filename = os.path.join(output_dir, "4D%s" % ext)
+                nibabel.save(film, film_filename)
+            else:
+                # save film as multiple filenames (3D vols), with ext extension
+                vols = nibabel.four_to_three(film)
+                film_filename = []
+                for t, vol in zip(xrange(n_scans), vols):
+                    vol_filename = os.path.join(output_dir,
+                                                "vol_%i%s" % (t, ext))
+                    nibabel.save(vol, vol_filename)
+                    film_filename.append(vol_filename)
+
+            # test loading proper
+            for t in xrange(n_scans):
+                # note that .img loads as Nifti1Pair, not Nifti1Image
+                vol_type = nibabel.Nifti1Pair if ext == '.img' else \
+                    nibabel.Nifti1Image
+
+                # load specific 3D vol from 4D film by filename
+                _vol, _n_scans = _load_specific_vol(film_filename, t)
+                nose.tools.assert_equal(_n_scans, n_scans)
+                nose.tools.assert_true(isinstance(_vol, vol_type))
+                nose.tools.assert_equal(_vol.shape, film.shape[:-1])
+                np.testing.assert_array_equal(_vol.get_data(),
+                                              film.get_data()[..., t])
+
+
+def test_save_vols():
+    # setup
+    n_scans = 10
+    output_dir = os.path.join(OUTPUT_DIR, inspect.stack()[0][3])
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # create 4D film
+    film = create_random_image(ndim=4, n_scans=n_scans)
+    threeD_vols = nibabel.four_to_three(film)
+
+    nose.tools.assert_equal(len(_save_vols(threeD_vols,
+                                           output_dir, ext='.nii.gz')),
+                            n_scans)
+
+
+def test_do_3Dto4D_merge():
+    # setup
+    n_scans = 10
+    output_dir = os.path.join(OUTPUT_DIR, inspect.stack()[0][3])
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # create 4D film
+    film = create_random_image(ndim=4, n_scans=n_scans)
+    threeD_vols = nibabel.four_to_three(film)
+
+    _film = do_3Dto4D_merge(threeD_vols)
+
+    nose.tools.assert_equal(_film.shape, film.shape)
+
+    _save_vols(threeD_vols, output_dir, ext='.nii.gz')
+
+
+# run all tests
+nose.runmodule(config=nose.config.Config(
+        verbose=2,
+        nocapture=True,
+        ))
