e65cfea0c30047f4ed6166196cbfe3de7b8eeba7
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2013-09-20 23:24:01 +0200

    upgraded utility module (for FSL preproc)

--- pypreprocess/nipype_preproc_fsl_utils.py
+++ pypreprocess/nipype_preproc_fsl_utils.py
@@ -1,128 +1,183 @@
 """
-Author: Bertrand Thirion, Alexandre Abraham
+Author: Bertrand Thirion, Alexandre Abraham, DOHMATOB Elvis Dopgima
+
 """
-# Standard imports
+
 import os
 import shutil
-
-# Nipype imports
-from nipype.caching import Memory
+from nipype.caching import Memory as NipypeMemory
+from joblib import Memory  as JoblibMemory
 import nipype.interfaces.fsl as fsl
-
-from nisl import datasets
+import commands
 
 fsl.FSLCommand.set_default_output_type('NIFTI_GZ')
-n_subjects = 3 
-
-# create directories where to write the image
-# for directory in ['/tmp/nyu/', '/tmp/nyu/fmri/', '/tmp/nyu/t1/']:
-#     if not os.path.exists(directory):
-#         os.mkdir(directory)
-
-# cmd_prefix = 'fsl5.0-'
-# nyu = datasets.fetch_nyu_rest(n_subjects=n_subjects)
-
-##############################################################
-# Preprocess functional data
-##############################################################
+FSL_T1_TEMPLATE = "/usr/share/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz"
 
 
-def do_subject_preproc(subject_output_dir,
-                         anat_image,
-                         fmri_images,
-                         cmd_prefix=None,
-                         **kwargs):
+def _get_file_ext(filename):
+    parts = filename.split('.')
+
+    return parts[0], ".".join(parts[1:])
+
+
+def _get_output_filename(input_filename, output_dir, output_prefix='',
+                         ext=None):
+    if isinstance(input_filename, basestring):
+        if not ext is None:
+            ext = "." + ext if not ext.startswith('.') else ext
+            input_filename = _get_file_ext(input_filename)[0] + ext
+
+        return os.path.join(output_dir,
+                            output_prefix + os.path.basename(input_filename))
+    else:
+        return [_get_output_filename(x, output_dir,
+                                     output_prefix=output_prefix)
+                for x in input_filename]
+
+
+def do_fsl_merge(in_files, output_dir, output_prefix='merged_',
+                 cmd_prefix="fsl5.0-"
+                 ):
+    output_filename = _get_output_filename(in_files[0], output_dir,
+                                           output_prefix=output_prefix,
+                                           ext='.nii.gz')
+
+    cmdline = "%sfslmerge -t %s %s" % (cmd_prefix, output_filename,
+                                       " ".join(in_files))
+    print cmdline
+    print commands.getoutput(cmdline)
+
+    return output_filename
+
+
+def do_subject_preproc(subject_id,
+                       output_dir,
+                       func,
+                       anat,
+                       do_bet=True,
+                       do_mc=True,
+                       do_coreg=True,
+                       do_normalize=True,
+                       cmd_prefix="fsl5.0-",
+                       **kwargs
+                       ):
+    """
+    Preprocesses subject data using FSL.
+
+    Parameters
+    ----------
+
+    """
+
+    output = {'func': func,
+              'anat': anat
+              }
+
+    # output dir
+    subject_output_dir = os.path.join(output_dir, subject_id)
+    if not os.path.exists(subject_output_dir):
+        os.makedirs(subject_output_dir)
 
     # prepare for smart-caching
-    t1_dir = os.path.join(subject_output_dir, 't1')
-    if not os.path.exists(t1_dir):
-        os.makedirs(t1_dir)
-    cache_dir = os.path.join(subject_output_dir, 'cache_dir')
+    cache_dir = os.path.join(output_dir, "cache_dir")
     if not os.path.exists(cache_dir):
         os.makedirs(cache_dir)
-    mem = Memory(base_dir=cache_dir)
 
+    nipype_mem = NipypeMemory(base_dir=cache_dir)
+    joblib_mem = JoblibMemory(cache_dir, verbose=100)
+
+    # sanitize input files
+    if not isinstance(output['func'], basestring):
+        output['func'] = joblib_mem.cache(do_fsl_merge)(
+            func, subject_output_dir, output_prefix='Merged',
+            cmd_prefix=cmd_prefix)
+
+    ######################
     #  Brain Extraction
-    bet = fsl.BET(
-            in_file=fmri_images,
-            out_file=os.path.join(subject_output_dir, 'fmri_bet.nii.gz'))
-    if cmd_prefix:
-        bet._cmd = cmd_prefix + bet._cmd
-    bet.run()
+    ######################
+    if do_bet:
+        if not fsl.BET._cmd.startswith("fsl"):
+            fsl.BET._cmd = cmd_prefix + fsl.BET._cmd
 
-    #  Slice timing correction
-    st = fsl.SliceTimer(
-            in_file=os.path.join(subject_output_dir, 'fmri_bet.nii.gz'),
-            out_file=os.path.join(subject_output_dir, 'fmri_st.nii.gz'))
-    st.inputs.interleaved = True
-    if cmd_prefix:
-        st._cmd = cmd_prefix + st._cmd
-    result_st = st.run()
+        bet = nipype_mem.cache(fsl.BET)
+        bet_results = bet(in_file=output['anat'],
+                          )
 
+        output['anat'] = bet_results.outputs.out_file
+
+    #######################
     #  Motion correction
-    mcflt = fsl.MCFLIRT(
-        in_file=os.path.join(subject_output_dir, 'fmri_st.nii.gz'),
-        cost='mutualinfo',
-        out_file = os.path.join(subject_output_dir, 'fmri_mc.nii.gz'))
-    if cmd_prefix:
-        mcflt._cmd = cmd_prefix + mcflt._cmd
-    mcflt.run()
+    #######################
+    if do_mc:
+        if not fsl.MCFLIRT._cmd.startswith("fsl"):
+            fsl.MCFLIRT._cmd = cmd_prefix + fsl.MCFLIRT._cmd
 
+        mcflirt = nipype_mem.cache(fsl.MCFLIRT)
+        mcflirt_results = mcflirt(in_file=output['func'],
+                                  cost='mutualinfo',
+                                  save_mats=True,  # save mc matrices
+                                  save_plots=True  # save mc params
+                                  )
+
+        output['motion_parameters'] = mcflirt_results.outputs.par_file
+        output['motion_matrices'] = mcflirt_results.outputs.mat_file
+        output['func'] = mcflirt_results.outputs.out_file
+
+    ###################
     # Coregistration
-    flt1 = fsl.FLIRT(in_file=os.path.join(subject_output_dir, 'fmri_mc.nii.gz'), 
-            reference=anat_image,
-            out_file=os.path.join(subject_output_dir, 'fmri_coreg_anat.nii.gz'),
-            out_matrix_file=os.path.join(subject_output_dir, 'fmri_flirt.mat'))
-    if cmd_prefix:
-        flt1._cmd = cmd_prefix + flt1._cmd
-    flt1.run()
+    ###################
+    if do_coreg:
+        if not fsl.FLIRT._cmd.startswith("fsl"):
+            fsl.FLIRT._cmd = cmd_prefix + fsl.FLIRT._cmd
 
+        flirt1 = nipype_mem.cache(fsl.FLIRT)
+        flirt1_results = flirt1(in_file=output['func'],
+                                reference=output['anat']
+                                )
 
-    #  T1 Spatial normalization
-    flt2 = fsl.FLIRT(in_file=anat_image, 
-            reference="/usr/share/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz",
-            out_file=os.path.join(t1_dir, 't1_warped.nii.gz'),
-            out_matrix_file=os.path.join(t1_dir, 't1_flirt.mat'))
-    if cmd_prefix:
-        flt2._cmd = cmd_prefix + flt2._cmd
-    flt2.run()
+        if not do_normalize:
+            output['func'] = flirt1_results.outputs.out_file
 
-    #  Concatenate transformations
-    concat = fsl.ConvertXFM(
-            in_file=os.path.join(subject_output_dir, 'fmri_flirt.mat'),
-            in_file2=os.path.join(t1_dir, 't1_flirt.mat'),
-            concat_xfm=True,
-            out_file=os.path.join(subject_output_dir, 'fmri_coreg.mat'))
-    if cmd_prefix:
-        concat._cmd = cmd_prefix + concat._cmd
-    concat.run()
+    ##########################
+    # Spatial normalization
+    ##########################
+    if do_normalize:
+        if not fsl.FLIRT._cmd.startswith("fsl"):
+            fsl.FLIRT._cmd = cmd_prefix + fsl.FLIRT._cmd
 
-    #  Functional/T1 coregistration
-    flt = fsl.ApplyXfm(
-            in_file=os.path.join(subject_output_dir, 'fmri_mc.nii.gz'),
-            in_matrix_file=os.path.join(subject_output_dir, 'fmri_coreg.mat'),
-            reference="/usr/share/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz",
-            out_file=os.path.join(subject_output_dir, 'fmri_coreg.nii.gz'),
-            out_matrix_file=os.path.join(subject_output_dir, 'fmri_final.mat'))
-    if cmd_prefix:
-        flt._cmd = cmd_prefix + flt._cmd
-    flt.run()
+        # T1 normalization
+        flirt2 = nipype_mem.cache(fsl.FLIRT)
+        flirt2_results = flirt2(in_file=output['anat'],
+                                reference=FSL_T1_TEMPLATE)
 
+        output['anat'] = flirt2_results.outputs.out_file
 
-# mainpath = '/tmp/nyu'
-# if not os.path.exists(mainpath):
-#     os.mkdir(mainpath)
-# for i, func in enumerate(nyu.func):
-#     path = os.path.join(mainpath, str(i))
-#     if not os.path.exists(path):
-#         os.mkdir(path)
-#     do_subject_preproc(path, nyu.anat_anon[i], func, cmd_prefix='fsl5.0-')
+        # concatenate 'func -> anat' and 'anat -> standard space'
+        # transformation matrices to obtaun 'func -> standard space'
+        # transformation matrix
+        if do_coreg:
+            if not fsl.ConvertXFM._cmd.startswith("fsl"):
+                fsl.ConvertXFM._cmd = cmd_prefix + fsl.ConvertXFM._cmd
 
-# """
-# fnt = fsl.FNIRT(in_file='/tmp/kr080082/t1/kr080082_t1.nii.gz', 
-#                 ref_file='/tmp/T1.nii', #ref_file=example_data('mni.nii'),
-#                 warped_file='/tmp/kr080082/t1/kr080082_t1_warped.nii.gz')
-# if neurospin:
-#     fnt._cmd = 'fsl4.1-' + fnt._cmd
-# res = fnt.run()
-# """
+                convertxfm = nipype_mem.cache(fsl.ConvertXFM)
+                convertxfm_results = convertxfm(
+                    in_file=flirt1_results.outputs.out_matrix_file,
+                    in_file2=flirt2_results.outputs.out_matrix_file,
+                    concat_xfm=True
+                    )
+
+        # warp func data into standard space by applying
+        # 'func -> standard space' transformation matrix
+        if not fsl.ApplyXfm._cmd.startswith("fsl"):
+            fsl.ApplyXfm._cmd = cmd_prefix + fsl.ApplyXfm._cmd
+
+        applyxfm = nipype_mem.cache(fsl.ApplyXfm)
+        applyxfm_results = applyxfm(
+            in_file=output['func'],
+            in_matrix_file=convertxfm_results.outputs.out_file,
+            reference=FSL_T1_TEMPLATE
+            )
+
+        output['func'] = applyxfm_results.outputs.out_file
+
+    return output
