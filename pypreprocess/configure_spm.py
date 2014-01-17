3f5d407405007cf807acda2e227d9c75167a8d4a
GaelVaroquaux <gael.varoquaux@normalesup.org>
2014-01-17 15:48:28 +0100

    Hardwire spm precompiled

--- pypreprocess/configure_spm.py
+++ pypreprocess/configure_spm.py
@@ -8,6 +8,8 @@
 import os
 import glob
 import nipype.interfaces.matlab as matlab
+from nipype.interfaces import spm
+
 from io_utils import _expand_path
 
 DEFAULT_SPM_DIR = '/i2bm/local/spm8'
@@ -15,6 +17,7 @@ DEFAULT_MATLAB_EXEC = "/neurospin/local/matlab/bin/matlab"
 
 
 def configure_spm(matlab_exec=None, spm_dir=None):
+    origin_spm_dir = spm_dir
 
     # configure SPM
     if 'SPM_DIR' in os.environ:
@@ -33,7 +36,13 @@ def configure_spm(matlab_exec=None, spm_dir=None):
         "Can't find SPM path '%s'! You should export SPM_DIR=/path/to/"
         "your/SPM/root/dir" % spm_dir)
 
-    matlab.MatlabCommand.set_default_paths(spm_dir)
+    if os.path.exists('/i2bm/local/bin/spm8') and origin_spm_dir is None:
+        matlab.MatlabCommand.set_default_paths(spm_dir)
+        matlab_cmd = ('/i2bm/local/bin/spm8 '
+                    'run script')
+        spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
+    else:
+        matlab.MatlabCommand.set_default_paths(spm_dir)
 
     # configure MATLAB
     if 'MATLAB_EXEC' in os.environ:
