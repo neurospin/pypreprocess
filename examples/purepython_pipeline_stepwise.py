b201e0022b742309ccce3438b7e8e50ef7ad28f2
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-03-26 20:55:26 +0100

    BF: coreg node didn't correctly prefix the output files (signaled by @michael)

--- examples/purepython_pipeline_stepwise.py
+++ examples/purepython_pipeline_stepwise.py
@@ -20,35 +20,6 @@ sd = fetch_spm_auditory_data(os.path.join(
         "CODE/datasets/spm_auditory"))
 n_sessions = 1  # this dataset has 1 session (i.e 1 fMRI acquisiton or run)
 
-# ouput dict
-output = {'func': [sd.func],  # one fMRI 4D datum per session
-          'anat': sd.anat, 'n_sessions': n_sessions}
-
-##################################
-# Slice-Timing Correction (STC)
-##################################
-for sess_func, sess_id in zip(output['func'], xrange(n_sessions)):
-    # session fit
-    fmristc = mem.cache(fMRISTC().fit)(raw_data=sess_func)
-
-    # session transform
-    output['func'][sess_id] = mem.cache(fmristc.transform)(sess_func)
-
-###########################
-# Motion Correction (MC)
-###########################
-mrimc = mem.cache(MRIMotionCorrection(n_sessions=n_sessions, quality=1.).fit)(
-    output['func'])
-mc_output = mem.cache(mrimc.transform)(reslice=True)
-
-output["func"] = mc_output['realigned_images']
-output["realignment_parameters"] = mc_output['realignment_parameters']
-
-###################
-# Coregistration
-###################
-coreg = mem.cache(Coregister().fit)(output["anat"], output['func'][0])
-output['func'] = [mem.cache(coreg.transform)(sess_func)
-                  for sess_func in output['func']]
-
-print output
+do_subject_preproc(sd.__dict__(), concat=False, coregister=True,
+                   stc=True, cv_tc=True, realign=True,
+                   report=True)
