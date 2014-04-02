07bd2c6e0ba99dd1961bfa3a2c1bc4662e1a71bb
Yannick Schwartz <yannick.schwartz@cea.fr>
2014-04-02 11:59:26 +0200

    configure_spm can use the environment variable SPM8_MCR to setup the compiled version of SPM

--- pypreprocess/configure_spm.py
+++ pypreprocess/configure_spm.py
@@ -44,7 +44,10 @@ def configure_spm(matlab_exec=None, spm_dir=None):
                 >= [0, 9] and
             os.path.exists('/i2bm/local/bin/spm8')
             and origin_spm_dir is None):
-        matlab_cmd = '/i2bm/local/bin/spm8 run script'
+        if 'SPM8_MCR' in os.environ:
+            matlab_cmd = '%s run script' % os.environ['SPM8_MCR']
+        else:
+            matlab_cmd = '/i2bm/local/bin/spm8 run script'
         spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
     else:
         matlab.MatlabCommand.set_default_paths(spm_dir)
