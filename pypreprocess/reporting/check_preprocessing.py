b96720d0258db3ac8c152515012c5456fff8c447
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-03-29 23:53:53 +0200

    BF: zombie imports

--- pypreprocess/reporting/check_preprocessing.py
+++ pypreprocess/reporting/check_preprocessing.py
@@ -13,9 +13,8 @@ import pylab as pl
 import nibabel
 from nilearn.plotting import plot_img, plot_stat_map
 from nilearn.image import reorder_img
-from ..external.nipy_labs import compute_mask_files
 from ..external import joblib
-from ..io_utils import load_4D_img, is_niimg, load_specific_vol
+from ..io_utils import load_4D_img, load_specific_vol
 
 EPS = np.finfo(float).eps
 
