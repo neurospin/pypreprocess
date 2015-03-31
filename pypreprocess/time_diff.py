11b3b9266a4f8ac6c2e9bb64766d3aa57f858068
Loïc Estève <loic.esteve@ymail.com>
2015-03-31 10:13:45 +0200

    Add plot_tsdiffs to pypreprocess/time_diff.py
    also tweak the ifmain to show a plot that should work as long as you have downloaded
    nyu_rest from nilearn

--- pypreprocess/time_diff.py
+++ pypreprocess/time_diff.py
@@ -12,17 +12,21 @@ This has been implemented in the Nipy package.
 We give here a simpler implementation with modified dependences
 
 '''
+import os
 
 import numpy as np
-from nilearn._utils import check_niimgs
+
 import nibabel as nib
+
+from nilearn.plotting import plot_stat_map
+from nilearn._utils import check_niimgs
 
 
 def time_slice_diffs(img):
     ''' Time-point to time-point differences over volumes and slices
 
     We think of the passed array as an image.
-    The last dimension is assumed to be time. 
+    The last dimension is assumed to be time.
 
     Parameters
     ----------
@@ -107,8 +111,90 @@ def time_slice_diffs(img):
             'slice_diff2_max_vol': slice_diff2_max_vol}
 
 
+def plot_tsdiffs(results, axes=None):
+    ''' Plotting routine for time series difference metrics
+
+    Requires matplotlib
+
+    Parameters
+    ----------
+    results : dict
+       Results of format returned from
+       :func:`pypreprocess.time_diff.time_slice_diff`
+    '''
+    import matplotlib.pyplot as plt
+
+    T = len(results['volume_means'])
+    S = results['slice_mean_diff2'].shape[1]
+    mean_means = np.mean(results['volume_means'])
+    scaled_slice_diff = results['slice_mean_diff2'] / mean_means
+
+    if axes is None:
+        fig, axes = plt.subplots(3, 2)
+        fig.set_size_inches(20, 10, forward=True)
+
+    def xmax_labels(ax, val, xlabel, ylabel):
+        xlims = ax.axis()
+        ax.axis((0, val) + xlims[2:])
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+    # plot of mean volume variance
+    ax = axes[0][0]
+    ax.plot(results['volume_mean_diff2'] / mean_means)
+    xmax_labels(ax, T - 1, 'Difference image number', 'Scaled variance')
+
+    # plot of diff by slice
+    ax = axes[1][0]
+    # Set up the color map for the different slices:
+    X, Y = np.meshgrid(np.arange(scaled_slice_diff.shape[0]),
+                       np.arange(scaled_slice_diff.shape[1]))
+
+    # Use HSV in order to code the slices from bottom to top:
+    ax.scatter(X.T.ravel(), scaled_slice_diff.ravel(),
+               c=Y.T.ravel(), cmap=plt.cm.hsv,
+               alpha=0.2)
+
+    xmax_labels(ax, T - 1,
+                'Difference image number',
+                'Slice by slice variance')
+
+    # mean intensity
+    ax = axes[2][0]
+    ax.plot(results['volume_means'] / mean_means)
+    xmax_labels(ax, T,
+                'Image number',
+                'Scaled mean \n voxel intensity')
+
+    # slice plots min max mean
+    ax = axes[0][1]
+    ax.hold(True)
+    ax.plot(np.mean(scaled_slice_diff, 0), 'k')
+    ax.plot(np.min(scaled_slice_diff, 0), 'b')
+    ax.plot(np.max(scaled_slice_diff, 0), 'r')
+    ax.hold(False)
+    xmax_labels(ax, S+1,
+                'Slice number',
+                'Max/mean/min \n slice variation')
+
+    ax = axes[1][1]
+    plot_stat_map(results['diff2_mean_vol'], bg_img=None,
+                  display_mode='z', cut_coords=5,
+                  axes=ax, black_bg=True, title='diff2_mean_vol')
+
+    ax = axes[2][1]
+    plot_stat_map(results['slice_diff2_max_vol'], bg_img=None,
+                  display_mode='z', cut_coords=5,
+                  axes=ax, black_bg=True, title='slice_diff2_max_vol')
+
+    return axes
+
+
 if __name__ == '__main__':
-    img = '/home/bertrand/retreat/results/ibc_001_01/fmri/rsocial_pa.nii'
-    plop = time_slice_diffs(img)
-    nib.save(plop['slice_diff2_max_vol'], '/tmp/max_diff.nii')
-    nib.save(plop['diff2_mean_vol'], '/tmp/mean_diff.nii')
+    import matplotlib.pyplot as plt
+
+    filename = '~/nilearn_data/nyu_rest/session1/sub22894/func/lfo.nii.gz'
+    filename = os.path.expanduser(filename)
+    results = time_slice_diffs(filename)
+    plot_tsdiffs(results)
+    plt.show()
