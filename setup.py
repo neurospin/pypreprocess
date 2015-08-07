8be06073516e953f73459aa0856116b0d07484d3
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-08-07 17:42:58 +0200

    restored setup.py to original state

--- setup.py
+++ setup.py
@@ -4,21 +4,32 @@ descr = """A set of python modules for neuroimaging..."""
 
 import sys
 import os
+import shutil
 
-from setuptools import setup, find_packages
+DISTNAME = 'pypreprocess'
+DESCRIPTION = 'Statistical learning for neuroimaging in Python'
+LONG_DESCRIPTION = open('README.rst').read()
+MAINTAINER = 'Gael Varoquaux'
+MAINTAINER_EMAIL = 'gael.varoquaux@normalesup.org'
+URL = 'http://pypreprocess.github.com'
+LICENSE = 'new BSD'
+DOWNLOAD_URL = 'http://pypreprocess.github.com'
+VERSION = '0.1-git'
+
+from numpy.distutils.core import setup
 
 
 def load_version():
-    """Executes pypreprocess/version.py in a globals dictionary and return it.
+    """Executes nilearn/version.py in a globals dictionary and return it.
 
-    Note: importing pypreprocess is not an option because there may be
+    Note: importing nilearn is not an option because there may be
     dependencies like nibabel which are not installed and
     setup.py is supposed to install them.
     """
     # load all vars into globals, otherwise
     #   the later function call using global vars doesn't work.
     globals_dict = {}
-    with open(os.path.join('pypreprocess', 'version.py')) as fp:
+    with open(os.path.join('nilearn', 'version.py')) as fp:
         exec(fp.read(), globals_dict)
 
     return globals_dict
@@ -30,33 +41,82 @@ def is_installing():
     return install_commands.intersection(set(sys.argv))
 
 
-# Make sources available using relative paths from this file's directory.
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# For some commands, use setuptools
+if len(set(('develop', 'sdist', 'release', 'bdist_egg', 'bdist_rpm',
+           'bdist', 'bdist_dumb', 'bdist_wininst', 'install_egg_info',
+           'build_sphinx', 'egg_info', 'easy_install', 'upload',
+            )).intersection(sys.argv)) > 0:
+    from setuptools import setup
 
-_VERSION_GLOBALS = load_version()
-DISTNAME = 'pypreprocess'
-DESCRIPTION = 'Statistical learning for neuroimaging in Python'
-LONG_DESCRIPTION = open('README.rst').read()
-MAINTAINER = 'Elvis DOHMATOB'
-MAINTAINER_EMAIL = 'elvis.dohmatob@inria.fr'
-URL = 'http://pypreprocess.github.io'
-LICENSE = 'new BSD'
-DOWNLOAD_URL = 'http://pypreprocess.github.io'
-VERSION = _VERSION_GLOBALS['__version__']
+
+def configuration(parent_package='', top_path=None):
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+
+    # main modules
+    config.add_subpackage('pypreprocess')
+
+    # spm loader
+    config.add_subpackage('pypreprocess/spm_loader')
+
+    # extrenal dependecies
+    config.add_subpackage('pypreprocess/external')
+    config.add_subpackage('pypreprocess/external/tempita')
+
+    # reporting plugin
+    config.add_subpackage('pypreprocess/reporting')
+    config.add_data_dir("pypreprocess/reporting/template_reports")
+    config.add_data_dir("pypreprocess/reporting/css")
+    config.add_data_dir("pypreprocess/reporting/js")
+    config.add_data_dir("pypreprocess/reporting/icons")
+    config.add_data_dir("pypreprocess/reporting/images")
+
+    return config
 
 
 if __name__ == "__main__":
-    if is_installing():
-        module_check_fn = _VERSION_GLOBALS['_check_module_dependencies']
-        module_check_fn(is_pypreprocess_installing=True)
 
-    install_requires = \
-        ['%s>=%s' % (mod, meta['min_version'])
-            for mod, meta in _VERSION_GLOBALS['REQUIRED_MODULE_METADATA']
-            if not meta['required_at_installation']]
+    old_path = os.getcwd()
+    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    # python 3 compatibility stuff.
+    # Simplified version of scipy strategy: copy files into
+    # build/py3k, and patch them using lib2to3.
+    if sys.version_info[0] == 3:
+        try:
+            import lib2to3cache
+        except ImportError:
+            pass
+        local_path = os.path.join(local_path, 'build', 'py3k')
+        if os.path.exists(local_path):
+            shutil.rmtree(local_path)
+        print("Copying source tree into build/py3k for 2to3 transformation"
+              "...")
+        shutil.copytree(os.path.join(old_path, 'pypreprocess'),
+                        os.path.join(local_path, 'pypreprocess'))
+        import lib2to3.main
+        from io import StringIO
+        print("Converting to Python3 via 2to3...")
+        _old_stdout = sys.stdout
+        try:
+            sys.stdout = StringIO()  # supress noisy output
+            res = lib2to3.main.main("lib2to3.fixes",
+                                    ['-x', 'import', '-w', local_path])
+        finally:
+            sys.stdout = _old_stdout
 
-    setup(name=DISTNAME,
+        if res != 0:
+            raise Exception('2to3 failed, exiting ...')
+
+    os.chdir(local_path)
+    sys.path.insert(0, local_path)
+
+    setup(configuration=configuration,
+          name=DISTNAME,
           maintainer=MAINTAINER,
+          include_package_data=True,
           maintainer_email=MAINTAINER_EMAIL,
           description=DESCRIPTION,
           license=LICENSE,
@@ -75,19 +135,6 @@ if __name__ == "__main__":
               'Topic :: Scientific/Engineering',
               'Operating System :: Microsoft :: Windows',
               'Operating System :: POSIX',
-              'Operating System :: Unix',
-              'Operating System :: MacOS',
-              'Programming Language :: Python :: 2',
-              'Programming Language :: Python :: 2.6',
-              'Programming Language :: Python :: 2.7',
-              'Programming Language :: Python :: 3.3',
-              'Programming Language :: Python :: 3.4',
-          ],
-          packages=find_packages(),
-          # package_data={'pypreprocess.datasets.data': ['*.nii.gz', '*.csv'],
-          #               'pypreprocess.plotting.glass_brain_files': ['*.json'],
-          #               'pypreprocess.tests.data': ['*'],
-          #               'pypreprocess.image.tests.data': ['*.mgz'],
-          #               'pypreprocess.datasets.tests.data': ['*.*'],
-          #               'pypreprocess.datasets.description': ['*.rst']},
-          install_requires=install_requires,)
+              'Operating System :: Unix'
+              ]
+          )
