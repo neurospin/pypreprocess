d18e03ec972ed81d4546d620d5f70e88337b16a8
KamalakerDadi <dkamalakarreddy@gmail.com>
2016-05-27 13:35:04 +0200

    Test to make sure version exists in pypreprocess

---
+++ pypreprocess/tests/test_version.py
@@ -0,0 +1,13 @@
+"""
+A simple test script for existance of pypreprocess version
+"""
+
+import pypreprocess
+
+from nose.tools import assert_true
+from ..version import __version__
+
+
+def test_version():
+    version = pypreprocess.__version__
+    assert_true(isinstance(version, basestring))
