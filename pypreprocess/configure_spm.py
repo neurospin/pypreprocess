0dc13161c1c3b145fb1b9ff7902ef1471cab9396
Jerome Dockes <jerome@dockes.org>
2016-06-01 17:04:29 +0200

    rectify directory which is considered to be spm home dir (must contain ./tpm), make recursive search possible

--- pypreprocess/configure_spm.py
+++ pypreprocess/configure_spm.py
@@ -8,6 +8,7 @@ import re
 import glob
 import distutils
 import logging
+import itertools
 
 from nipype.interfaces import matlab
 from nipype.interfaces import spm
@@ -56,32 +57,43 @@ def _check_nipype_version():
         return False
     return True
 
-def _find_or_warn(loc, check, msg=None, warner=_logger.warn):
+def _find_or_warn(loc, check, msg=None, warner=_logger.warn, recursive=False):
     if loc is None:
-        return False
+        return None
+    loc = os.path.abspath(os.path.expanduser(loc))
     if check(loc):
-        return True
+        return loc
+    if(recursive and os.path.isdir(loc)):
+        for (dirpath, dirnames, filenames) in os.walk(loc):
+            for name in itertools.chain(dirnames, filenames):
+                found_path = os.path.join(dirpath, name)
+                if check(found_path):
+                    return found_path
     if msg is not None:
         warner(msg)
-    return False
+    return None
 
 def _find_dep_loc(specified_loc=None, exported_name=None, default_locs=None,
-                  check=os.path.exists, msg_prefix=''):
+                  check=os.path.exists, msg_prefix='', recursive=False):
 
-    if _find_or_warn(specified_loc, check, ''.join(
-            [msg_prefix, ' specified location: "{}"'.format(specified_loc)])):
-        return specified_loc
+    message = '{} specified location: "{}"'.format(msg_prefix, specified_loc)
+    found = _find_or_warn(specified_loc, check, msg=message,
+                          recursive=recursive)
+    if found is not None:
+        return found
 
     exported_loc = os.environ.get(exported_name)
-    if _find_or_warn(exported_loc, check, ''.join(
-            [msg_prefix, ' exported location: "{}"'.format(exported_loc)])):
-        return exported_loc
+    message = '{} exported location: "{}"'.format(msg_prefix, exported_loc)
+    found = _find_or_warn(exported_loc, check, msg=message, recursive=recursive)
+    if found is not None:
+        return found
 
     for def_loc in default_locs:
-        if _find_or_warn(def_loc, check, ''.join(
-                [msg_prefix, ' default location: "{}"'.format(def_loc)]),
-                         warner=_logger.debug):
-            return def_loc
+        message = '{} default location: "{}"'.format(msg_prefix, def_loc)
+        found = _find_or_warn(def_loc, check, msg=message,
+                              recursive=recursive, warner=_logger.debug)
+        if found is not None:
+            return found
 
     return None
 
@@ -93,19 +105,43 @@ def _find_spm_mcr(spm_mcr):
         spm_mcr, 'SPM_MCR', DEFAULT_SPM_MCRS,
         check=_is_executable,
         msg_prefix='SPM MCR is not executable or could not be found in')
-    if spm_mcr is not None:
-        if _check_nipype_version():
-            _logger.info('setting SPM MCR backend: '
-                         'set matlab executable path to "{}" '
-                         'and "use_mcr" to True'.format(spm_mcr))
-            spm.SPMCommand.set_mlab_paths(
-                matlab_cmd='{} run script'.format(spm_mcr),
-                use_mcr=True)
-            return spm_mcr
-        _logger.warn(
-            'nipype version {} too old.'
-            ' No support for precompiled SPM'.format(nipype.__version__))
+    if spm_mcr is None:
         return None
+
+    if _check_nipype_version():
+        _logger.info('setting SPM MCR backend: '
+                     'set matlab executable path to "{}" '
+                     'and "use_mcr" to True'.format(spm_mcr))
+        spm.SPMCommand.set_mlab_paths(
+            matlab_cmd='{} run script'.format(spm_mcr),
+            use_mcr=True)
+        DEFAULT_SPM_DIRS.insert(0, os.path.dirname(spm_mcr))
+        return spm_mcr
+    _logger.warn(
+        'nipype version {} too old.'
+        ' No support for precompiled SPM'.format(nipype.__version__))
+    return None
+
+def _is_spm_dir(dir_path):
+    return(os.path.isdir(dir_path) and 'tpm' in os.listdir(dir_path))
+
+def _find_spm_dir(spm_dir):
+    return _find_dep_loc(
+        spm_dir, 'SPM_DIR', DEFAULT_SPM_DIRS,
+        check=_is_spm_dir,
+        msg_prefix='SPM directory could not be found in',
+        recursive=True)
+
+def _find_spm_mcr_and_spm_dir(spm_mcr, spm_dir):
+    spm_mcr = _find_spm_mcr(spm_mcr)
+    if spm_mcr is None:
+        return None
+
+    spm_dir = _find_spm_dir(spm_dir)
+    if spm_dir is not None:
+        _logger.info('found SPM directory to be "{}"'.format(spm_dir))
+        return spm_dir
+    return None
 
 def _find_matlab_exec_and_spm_dir(spm_dir, matlab_exec):
     matlab_exec = _find_dep_loc(
@@ -113,18 +149,17 @@ def _find_matlab_exec_and_spm_dir(spm_dir, matlab_exec):
         'MATLAB_EXEC', DEFAULT_MATLAB_EXECS,
         check=_is_executable,
         msg_prefix='matlab executable file could not be found in')
-    if matlab_exec is not None:
-        spm_dir = _find_dep_loc(
-            spm_dir, 'SPM_DIR', DEFAULT_SPM_DIRS,
-            check=os.path.isdir,
-            msg_prefix='SPM directory could not be found in')
-        if spm_dir is not None:
-            _logger.info('setting default matlab command to be "{}"'.format(
-                matlab_exec))
-            matlab.MatlabCommand.set_default_matlab_cmd(matlab_exec)
-            _logger.info('setting matlab default paths to "{}"'.format(spm_dir))
-            matlab.MatlabCommand.set_default_paths(spm_dir)
-            return spm_dir
+    if matlab_exec is None:
+        return None
+
+    spm_dir = _find_spm_dir(spm_dir)
+    if spm_dir is not None:
+        _logger.info('setting default matlab command to be "{}"'.format(
+            matlab_exec))
+        matlab.MatlabCommand.set_default_matlab_cmd(matlab_exec)
+        _logger.info('setting matlab default paths to "{}"'.format(spm_dir))
+        matlab.MatlabCommand.set_default_paths(spm_dir)
+        return spm_dir
     return None
 
 def _configure_spm(spm_dir=None, matlab_exec=None, spm_mcr=None):
@@ -140,14 +175,13 @@ def _configure_spm(spm_dir=None, matlab_exec=None, spm_mcr=None):
     then the corresponding environment variable,
     then in some default locations.
     """
-    spm_mcr = _find_spm_mcr(spm_mcr)
-    if spm_mcr is not None:
-        spm_dir =  os.path.abspath(os.path.dirname(spm_mcr))
-        return spm_dir
+    found_spm_dir = _find_spm_mcr_and_spm_dir(spm_mcr, spm_dir)
+    if found_spm_dir is not None:
+        return found_spm_dir
 
-    spm_dir = _find_matlab_exec_and_spm_dir(spm_dir, matlab_exec)
-    if spm_dir is not None:
-        return spm_dir
+    found_spm_dir = _find_matlab_exec_and_spm_dir(spm_dir, matlab_exec)
+    if found_spm_dir is not None:
+        return found_spm_dir
 
     _logger.error('could not find SPM_MCR precompiled executable'
                   ' nor a pair (Matlab executable and SPM directory)')
@@ -158,7 +192,7 @@ def _get_version_spm(spm_dir):
     """
     _, spm_version = os.path.split(spm_dir)
     if not spm_version:
-        _loggers.warn('Could not figure out SPM version!'
+        _logger.warn('Could not figure out SPM version!'
                      ' (spm dir: "{}")'.format(spm_dir))
         return
     return spm_version
