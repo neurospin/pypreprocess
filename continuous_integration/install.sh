6473543ea80f04556ce54694b53b8c318b8ac35c
Loïc Estève <loic.esteve@ymail.com>
2017-01-17 19:53:30 +0100

    TRAVIS add Python 3 build

---
+++ continuous_integration/install.sh
@@ -0,0 +1,69 @@
+set -e
+
+print_conda_requirements() {
+    # Echo a conda requirement string for example
+    # "pip python=2.7.3 scikit-learn=*". It has a hardcoded
+    # list of possible packages to install and looks at _VERSION
+    # environment variables to know whether to install a given package and
+    # if yes which version to install. For example:
+    #   - for numpy, NUMPY_VERSION is used
+    #   - for scikit-learn, SCIKIT_LEARN_VERSION is used
+    TO_INSTALL_ALWAYS="pip nose"
+    REQUIREMENTS="$TO_INSTALL_ALWAYS"
+    TO_INSTALL_MAYBE="python numpy scipy scikit-learn pandas matplotlib networkx flake8"
+    for PACKAGE in $TO_INSTALL_MAYBE; do
+        # Capitalize package name and add _VERSION
+        PACKAGE_VERSION_VARNAME="${PACKAGE^^}_VERSION"
+        # replace - by _, needed for scikit-learn for example
+        PACKAGE_VERSION_VARNAME="${PACKAGE_VERSION_VARNAME//-/_}"
+        # dereference $PACKAGE_VERSION_VARNAME to figure out the
+        # version to install
+        PACKAGE_VERSION="${!PACKAGE_VERSION_VARNAME}"
+        if [ -n "$PACKAGE_VERSION" ]; then
+            REQUIREMENTS="$REQUIREMENTS $PACKAGE=$PACKAGE_VERSION"
+        fi
+    done
+    echo $REQUIREMENTS
+}
+
+create_new_conda_env() {
+    # Deactivate the travis-provided virtual environment and setup a
+    # conda-based environment instead
+    deactivate
+
+    # Use the miniconda installer for faster download / install of conda
+    # itself
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
+         -O miniconda.sh
+    chmod +x miniconda.sh && ./miniconda.sh -b
+    export PATH=/home/travis/miniconda2/bin:$PATH
+    conda update --yes conda
+
+    # Configure the conda environment and put it in the path using the
+    # provided versions
+    REQUIREMENTS=$(print_conda_requirements)
+    echo "conda requirements string: $REQUIREMENTS"
+    conda create -n testenv --yes $REQUIREMENTS
+    source activate testenv
+}
+
+if [[ "$DISTRIB" == "neurodebian" ]]; then
+    bash <(wget -q -O- http://neuro.debian.net/_files/neurodebian-travis.sh)
+    sudo apt-get install -qq python-scipy python-nose python-nibabel python-sklearn python-pandas python-networkx python-nipype
+    sudo apt-get install -qq python-pip
+    pip install "nilearn>=0.1.3"
+elif [[ "$DISTRIB" == "conda" ]]; then
+    create_new_conda_env
+
+    # dependencies that are only available through pip
+    pip install nilearn nipype configobj
+else
+    echo "Unknown distrib: $DISTRIB"
+    exit 1
+fi
+
+python setup.py install
+
+if [[ "$COVERAGE" == "true" ]]; then
+    pip install coverage coveralls
+fi
