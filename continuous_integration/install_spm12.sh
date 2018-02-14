423ae4f9492fe852419e11e569d49f8bedf54bc7
chrplr <christophe@pallier.org>
2018-02-14 11:29:37 +0100

    warn about installing libXp.so.6 if it is missing

--- continuous_integration/install_spm12.sh
+++ continuous_integration/install_spm12.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Time-stamp: <2017-06-04 16:02:14 cp983411>
+# Time-stamp: <2018-02-14 11:26:59 cp983411>
 
 # Download and install SPM12 standalone (no Matlab license required)
 # see https://en.wikibooks.org/wiki/SPM/Standalone
@@ -19,9 +19,8 @@ fi
 
 mkdir -p $SPM_ROOT_DIR
 
-SPM_ROOT_DIR=~/opt/spm12   #  Installation directory
-
-SPM_SRC=spm12_r7219.zip  # this used to be spm12_r6914.zip, watch out it might get pulled down again!!!
+# Download
+SPM_SRC=spm12_r????.zip
 MCRINST=MCRInstaller.bin
 
 wget -N -r -l1 --no-parent -nd  -P $SPM_ROOT_DIR -A.zip -R "index.html*" http://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/
@@ -29,11 +28,10 @@ wget -N -r -l1 --no-parent -nd  -P $SPM_ROOT_DIR -A.zip -R "index.html*" http://
 wget -N -r -l1 --no-parent -nd  -P $SPM_ROOT_DIR -A.bin http://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/MCR/glnxa64/
 
 
-if [ ! -d spm12 ]; then
-    if [ ! -f ${SPM_SRC} ]; then
-	wget http://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/${SPM_SRC}
-    fi
-fi
+# Install
+cd $SPM_ROOT_DIR
+unzip -q -u ${SPM_SRC}
+chmod 755 spm12/run_spm12.sh
 
 if [ ! -d mcr ]; then
    chmod 755 ${MCRINST}
@@ -49,10 +47,25 @@ EOF
 
 chmod 755 spm12.sh
 
+if [ ! -f /usr/lib/x86_64-linux-gnu/libXp.so.6 ]; then
+    echo "WARNING!!!"
+    echo "/usr/lib/x86_64-linux-gnu/libXp.so.6 is missing"
+    echo 
+    echo To install it:
+    echo 'sudo add-apt-repository "deb http://securuty.ubuntu.com/ubuntu precise-security main"'
+    echo 'sudo apt update'
+    echo 'sudo apt install lixp6'
+    echo 'sudo add-apt-repository -r "deb http://securuty.ubuntu.com/ubuntu precise-security main"'
+fi
+
 # Create CTF
 ${SPM_ROOT_DIR}/spm12.sh quit
 cmds="export SPM_DIR=$SPM_ROOT_DIR/spm12/; export SPM_MCR=$SPM_ROOT_DIR/spm12.sh"
 ${cmds}
-echo "You may want to add the following commands (the exports) to your ~/.bashrc file once and for all."
 echo
 echo ${cmds}
+echo "IMPORTANT: you should now execute the following line: "
+echo "echo ${cmds} >> $HOME/.bashrc"
+
+
+
